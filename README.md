304230aad0ae12fe056082a337011ea6cabee984
FrancoUxoo <marmafafranco@gmail.com>
2026-06-17 19:17:04 -0400

    Update and rename README.md to Tarea.py

--- README.md
+++
@@ -1,5 +0,0 @@
-# Python_Pro17-06-2026
-
-# ESTE REPOSITORIO FUE CREADO POR UXO.
-
-# este repositorio fue creado para recopilar informacion clave de mi proyecto piloto creado, gestionarlo y avanzar de manera eficiente y coordinada con mis compañeros.
---
+++ Tarea.py
@@ -0,0 +1,15 @@
+# Python_Pro17-06-2026
+
+# ESTE REPOSITORIO FUE CREADO POR UXO.
+
+import random
+datos = [
+    "Mi aficion favorita es pasar el tiempo con mis amigos.",
+    "Mi proyecto favorito fue crear un juego estilo galaga/spaceinvaders.",
+    "Me gusta escuchar musica mientras trabajo en mis cosas personales.",
+    "Tengo una perro de mascota y se llama perla.",
+    "Mi comida favorita es el arroz con pollo acaramelado."
+    ]
+
+datos_aleatorio = random.choice(datos)
+print(datos_aleatorio)
